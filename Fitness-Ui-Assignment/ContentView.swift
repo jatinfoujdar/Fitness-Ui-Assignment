ef1925af3daaf151a940bb00743f1357aca32e5a
Jatin Foujdar <jatin321001@gmail.com>
2024-11-11 22:09:44 +0530

    Initial Commit

---
+++ Fitness-Ui-Assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fitness-Ui-Assignment
+//
+//  Created by jatin foujdar on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fitness-Ui-Assignment/Fitness_Ui_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_Ui_AssignmentApp.swift
+//  Fitness-Ui-Assignment
+//
+//  Created by jatin foujdar on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_Ui_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
