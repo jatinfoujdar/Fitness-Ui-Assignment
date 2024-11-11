ea88f839d3fd48736a09f7a2e95705d949e65e26
Jatin Foujdar <jatin321001@gmail.com>
2024-11-11 23:14:35 +0530

    background color commit

--- Fitness-Ui-Assignment/ContentView.swift
+++ Fitness-Ui-Assignment/ContentView.swift
@@ -1,21 +1,24 @@
-//
-//  ContentView.swift
-//  Fitness-Ui-Assignment
-//
-//  Created by jatin foujdar on 11/11/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            
+            Color(red: 0.204, green: 0.216, blue: 0.247)
+                .overlay(
+                    Text("Fitness Courses")
+                        .font(.system(size:30, weight: .bold, design: .default))
+                        .foregroundColor(.white)
+                        .padding(.bottom,200)
+                        
+                )
+                .frame(height: UIScreen.main.bounds.height / 2)
+
+          
+            Color.white
+                .frame(height: UIScreen.main.bounds.height / 2)
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
