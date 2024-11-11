c849c6e3d39fb16a26cfad361e55474227bf5ca3
Jatin Foujdar <jatin321001@gmail.com>
2024-11-12 00:06:01 +0530

    align commit

--- Fitness-Ui-Assignment/ContentView.swift
+++ Fitness-Ui-Assignment/ContentView.swift
@@ -6,15 +6,23 @@ struct ContentView: View {
             
             Color(red: 0.204, green: 0.216, blue: 0.247)
                 .overlay(
-                    Text("Fitness Courses")
-                        .font(.system(size:30, weight: .bold, design: .default))
-                        .foregroundColor(.white)
-                        .padding(.bottom,200)
+                    VStack(alignment: .leading) {
+                        Text("Fitness Courses")
+                            .font(.system(size: 35, weight: .bold, design: .default))
+                            .foregroundColor(.white)
+                            .padding(.bottom,150)
+
+                        Text("Your Activity Lessons")
+                            .font(.system(size: 20, weight: .bold, design: .default))
+                            .foregroundColor(.white)
+                            
                         
+                        
+                    }
+                   
                 )
                 .frame(height: UIScreen.main.bounds.height / 2)
 
-          
             Color.white
                 .frame(height: UIScreen.main.bounds.height / 2)
         }
