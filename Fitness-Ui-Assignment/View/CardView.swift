b8d26d0a7cf81929bcf92bd661189806c93c97f5
Jatin Foujdar <jatin321001@gmail.com>
2024-11-11 22:36:33 +0530

    file commit

---
+++ Fitness-Ui-Assignment/View/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Fitness-Ui-Assignment
+//
+//  Created by jatin foujdar on 11/11/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ Fitness-Ui-Assignment/View/CategoriesView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesView.swift
+//  Fitness-Ui-Assignment
+//
+//  Created by jatin foujdar on 11/11/24.
+//
+
+import SwiftUI
+
+struct CategoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoriesView()
+}
